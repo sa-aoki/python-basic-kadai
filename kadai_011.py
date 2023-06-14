0cbe647c699088adb6e09eec66827e78f801c9fe
Sanae Aoki <134927378+sa-aoki@users.noreply.github.com>
2023-06-14 17:32:39 +0900

    kadai_011.pyアップロード

--- kadai_011.py
+++ kadai_011.py
@@ -1,1 +1,13 @@
+planets = ["水","金","地","火","木","土","天","海","冥"]
 
+#for
+for planet in planets:
+    print(planet)
+
+
+#while
+i = 0
+
+while i < len(planets):
+    print(planets[i])
+    i = i + 1
