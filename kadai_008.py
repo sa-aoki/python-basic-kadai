294d06e56a6c75626a51cf15092b650a11e24bd2
Sanae Aoki <134927378+sa-aoki@users.noreply.github.com>
2023-06-14 15:41:45 +0900

    kadai_008.pyアップロード

--- kadai_008.py
+++ kadai_008.py
@@ -1,1 +1,20 @@
+import random
 
+var = random.randint(0,30)
+
+print(var)
+
+
+if var % 3 == 0 and var % 5 != 0 :
+    print("Fizz")
+
+elif var % 5 == 0 and var % 3 != 0:
+    print("Bazz")
+
+elif var % 3 == 0 and var % 5 == 0:
+    print("FizzBazz")
+
+else: 
+    print(var)
+
+    
